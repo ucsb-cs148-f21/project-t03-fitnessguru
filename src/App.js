75e98a9ff5652a41f736b65352f213539dde68e4
Martin Cabello <m_cabello@ucsb.edu>
2021-10-07 16:08:43 -0700

    Hello World App Added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header className="App-header">
+        <h1>Hello, World!</h1>
+        <p>Martin Cabello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
