4442f8ba01eef49baf07ae0b8dca382b9a970891
Kevin Weng <kweng@umail.ucsb.edu>
2021-10-12 13:18:09 -0700

    customized the app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Container>
       App developed for CS148 S21. Check out the source code{" "}
-      <a href="https://github.com/vincentktieu101/ReactGoogleAuthStarter">
+      <a href="https://github.com/ucsb-cs148-f21/project-t03-fitnessguru">
         here
       </a>
       .
