94f8f0716fbbd8a95db2c2db1151406f7d25d764
Kevin Weng <kweng@umail.ucsb.edu>
2021-10-25 20:49:22 -0700

    fixed a bug

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -14,6 +14,97 @@ export default function Exercise() {
 
   return (
     <div>
+      <button
+        type="button"
+        class="btn btn-primary btn-block"
+        data-toggle="modal"
+        data-target="#addExercise"
+      >
+        Add Exercise
+      </button>
+      <br />
+      <div
+        class="modal fade"
+        id="addExercise"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Add Exercise
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form action="/exercises" method="POST" class="mb-4">
+              <div class="modal-body">
+                <div class="form-group">
+                  <input
+                    type="hidden"
+                    name="googleId"
+                    value={user.id}
+                    class="form-control"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exercise">Exercise</label>
+                  <input type="text" name="name" class="form-control" />
+                </div>
+                <br />
+                <div class="form-group">
+                  <label for="sets">Sets</label>
+                  <input type="number" name="sets" class="form-control" />
+                </div>
+                <br />
+                <div class="form-group">
+                  <label for="repetitions">Repetitions</label>
+                  <input
+                    type="number"
+                    name="repetitions"
+                    class="form-control"
+                  />
+                </div>
+                <br />
+                <div class="form-group">
+                  <label for="weight">Weight</label>
+                  <input type="number" name="weight" class="form-control" />
+                </div>
+                <br />
+                <div class="form-group">
+                  <label for="notes">Notes</label>
+                  <input type="text" name="notes" class="form-control" />
+                </div>
+                <br />
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <input
+                  type="submit"
+                  value="Save Exercise"
+                  class="btn btn-primary btn-block"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <br />
       <table class="table table-bordered">
         <tr>
           <th>Exercise</th>
@@ -36,7 +127,7 @@ export default function Exercise() {
                   type="button"
                   class="btn btn-primary btn-block"
                   data-toggle="modal"
-                  data-target="#updateExercise"
+                  data-target={"#updateExercise" + exercise._id}
                 >
                   Update
                 </button>
@@ -44,7 +135,7 @@ export default function Exercise() {
 
               <div
                 class="modal fade"
-                id="updateExercise"
+                id={"updateExercise" + exercise._id}
                 tabindex="-1"
                 role="dialog"
                 aria-labelledby="exampleModalLabel"
