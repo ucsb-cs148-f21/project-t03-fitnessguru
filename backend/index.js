b3d00ac4e89832e748701edddf4beab7531cdf41
Kevin Weng <kweng@umail.ucsb.edu>
2021-10-26 19:45:11 -0700

    testing changes on Heroku

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require('path')
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../build')))
-
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
@@ -20,13 +17,16 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
 app.use("/user", require("./config/routes/user"));
 app.use("/exercises", require("./config/routes/exercise"));
 app.use("/workouts", require("./config/routes/workout"));
 app.use("/splits", require("./config/routes/split"));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../build/index.html'))
+    res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
   })
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
