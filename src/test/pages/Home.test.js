9cf2fce5c911b11c6e87316430e50bec17ec9fed
Martin Cabello <m_cabello@ucsb.edu>
2021-10-27 17:10:03 -0700

    test

---
+++ src/test/pages/Home.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../../pages/Home";
+
+describe("Home tests", () => {
+    test("renders without crashing", () => {
+      render(<Home />);
+    });
+  });
+  
