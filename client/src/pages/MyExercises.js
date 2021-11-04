113068ca91b3cf288b879dd52d387d5a9e17e77e
Michael Curtis <mikey1030@gmail.com>
2021-11-04 14:13:21 -0700

    styling

--- client/src/pages/MyExercises.js
+++ client/src/pages/MyExercises.js
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import Exercise from "../components/Exercise";
 import Dropdown from "react-bootstrap/Dropdown";
 import axios from "axios";
+import "./MyExercises.css";
 
 const Exx = ({exercise}) => {
   return(
@@ -17,40 +18,73 @@ const Exx = ({exercise}) => {
   )
 }
 
+const ExerciseCatDropDown = ({exercises, title}) => {
+  return(
+  <>
+  <Dropdown id="Primary">
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      {title}
+    </Dropdown.Toggle>
+  
+    <Dropdown.Menu>
+      {exercises.map(exercise => <Dropdown.Item onClick={() => console.log("hi")}>{exercise.name}</Dropdown.Item>)}
+    </Dropdown.Menu>
+  </Dropdown>
+  </>
+  )
+}
+
 export default function MyExercises() {
   const user = getUser();
-  const [exercises,setExercises] = useState();
+  const [arms,setArms] = useState();
+  const [legs, setLegs] = useState();
+  const [abs, setAbs] = useState();
+  const [chest,setChest] = useState();
+  const [back, setBack] = useState();
+  const [shoulders,setShoulders] = useState();
+  const [calves, setCalves] = useState();
 
-  const handleGetExercises = (category) => {
-    const url = `https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=${category}`
-    axios.get(url)
-      .then((res) => setExercises(res.data.results))
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=8")
+      .then((res) => setArms(res.data.results))
       .catch((error) => console.log(error))
-  }
 
-  console.log("test");
-  console.log(exercises)
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=9")
+      .then((res) => setLegs(res.data.results))
+      .catch((error) => console.log(error))
+
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=10")
+      .then((res) => setAbs(res.data.results))
+      .catch((error) => console.log(error))
+
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=11")
+      .then((res) => setChest(res.data.results))
+      .catch((error) => console.log(error))
+
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=12")
+      .then((res) => setBack(res.data.results))
+      .catch((error) => console.log(error))
+
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=13")
+      .then((res) => setShoulders(res.data.results))
+      .catch((error) => console.log(error))
+
+  axios.get("https://wger.de/api/v2/exercise/?limit=100&offset=0&language=2&category=14")
+      .then((res) => setCalves(res.data.results))
+      .catch((error) => console.log(error))
+
   return (
     <Layout user={user}>
       <Container>
-        <Exercise />        
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Exercises
-          </Dropdown.Toggle>
-        
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={() => handleGetExercises(8)}>Arms</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(9)}>Legs</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(10)}>Abs</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(11)}>Chest</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(12)}>Back</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(13)}>Shoulders</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGetExercises(14)}>Calves</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-
-      {exercises && exercises.map(exercise => <Exx exercise={exercise}/>)}
+      <div id="categories">  
+        {arms && <ExerciseCatDropDown className="category" exercises={arms} title="Arms" />}
+        {legs && <ExerciseCatDropDown className="category" exercises={legs} title="Legs" />}
+        {chest && <ExerciseCatDropDown className="category" exercises={chest} title="chest" />}
+        {back && <ExerciseCatDropDown className="category" exercises={back} title="back" />}
+        {abs && <ExerciseCatDropDown className="category" exercises={abs} title="abs" />}
+        {shoulders && <ExerciseCatDropDown className="category" exercises={shoulders} title="shoulders" />}
+        {calves && <ExerciseCatDropDown className="category" exercises={calves} title="calves" />}
+        </div>  
+        <Exercise />    
         
       </Container>
     </Layout>
